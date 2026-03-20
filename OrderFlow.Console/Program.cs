eebc1b06728c0b22cc0b474cf33c94a5194bf3fb
AKuba451 <Kuboks2017@gmail.com>
2026-03-20 18:14:56 +0100

    Zadanie 3 Skończone

--- OrderFlow.Console/Program.cs
+++ OrderFlow.Console/Program.cs
@@ -2,7 +2,7 @@
 using ConsoleApp1.Data;
 using ConsoleApp1.Models;
 using ConsoleApp1.Services;
-
+// POCZATEK ZADANIA 2
 var validator = new OrderValidator();
 
 var validOrder = SampleData.Orders[0];
@@ -46,3 +46,40 @@ else
 {
     invalidResult.ForEach(Console.WriteLine);
 }
+
+// KONIEC ZADANIA 2
+
+// POCZATEK ZADANIA 3
+var processor = new OrderProcessor();
+var allOrders = SampleData.Orders;
+
+var expensiveOrders = processor.FilterOrders(allOrders, o => o.TotalAmount > 1000);
+var vipOrders = processor.FilterOrders(allOrders, o=> o.Customer.VIP);
+var newOrders = processor.FilterOrders(allOrders, o => o.Status == OrderStatus.New);
+
+Action<Order> printSummary = o => Console.WriteLine($"ID: {o.ID}, Customer: {o.Customer.Name}, Total:  {o.TotalAmount:C}");
+Action<Order> markAsProcessing = o => o.Status = OrderStatus.Processing;
+
+Console.WriteLine("\n--- PRINT : ZADANIE 3 ---");
+processor.ProcessOrders(newOrders, printSummary);
+
+var shortSummaries = processor.ProjectOrders(allOrders, o => new
+{
+    OrderID = o.ID,
+    CustomerName = o.Customer.Name,
+    ItemCount = o.Items.Count,
+});
+
+decimal total = processor.AggregateOrders(allOrders, items => items.Sum(o => o.TotalAmount));
+decimal average = processor.AggregateOrders(allOrders, items => items.Average(o => o.TotalAmount));
+decimal max = processor.AggregateOrders(allOrders, items => items.Max(o => o.TotalAmount));
+
+Console.WriteLine($"\nSTATS: TOTAL: {total:C}, AVERAGE: {average:C}, MAX: {max:C}");
+
+Console.WriteLine("\n--- ODPALENIE : ZADANIE 3 ---");
+allOrders
+    .Where(o => o.Status != OrderStatus.Cancelled)
+    .OrderByDescending(o => o.TotalAmount)
+    .Take(3)
+    .ToList()
+    .ForEach(o => Console.WriteLine($"TOP - ID: {o.ID}, TOTAL: {o.TotalAmount:C}"));
---
+++ OrderFlow.Console/Services/OrderProcessor.cs
@@ -0,0 +1,34 @@
+﻿using ConsoleApp1.Models;
+
+namespace ConsoleApp1.Services;
+
+public class OrderProcessor
+{
+    public List<Order> FilterOrders(List<Order> orders, Predicate<Order> condition)
+    {
+        return orders.FindAll(condition);
+    }
+
+    public void ProcessOrders(List<Order> orders, Action<Order> action)
+    {
+        foreach (var order in orders)
+        {
+            action(order);
+        }
+    }
+
+    public List<T> ProjectOrders<T>(List<Order> orders, Func<Order, T> selector)
+    {
+        var results = new List<T>();
+        foreach (var order in orders)
+        {
+            results.Add(selector(order));
+        }
+        return results;
+    }
+
+    public decimal AggregateOrders(List<Order> orders, Func<IEnumerable<Order>, decimal> aggregator)
+    {
+        return aggregator(orders);
+    }
+}
