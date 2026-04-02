4ed487a40c62c3f24c64e45f27011d7413a56e36
igorptak557788 <igor.ptak1@wp.pl>
2026-04-02 18:53:21 +0200

    Dodanie Pliku Events

---
+++ OrderFlow.Console/Models/Events.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1.Models;
+
+public class OrderStatusChangedEventArgs : EventArgs
+{
+    public Order Order { get; } 
+    public OrderStatus OldStatus { get; }
+    public OrderStatus NewStatus { get; }
+
+    public OrderStatusChangedEventArgs(Order order, OrderStatus oldStatus, OrderStatus newStatus)
+    {
+        Order = order;
+        OldStatus = oldStatus;
+        NewStatus = newStatus;
+        
+    }
+}
+
+public class OrderValidationEventArgs : EventArgs
+{
+    public Order Order { get; }
+    public bool isValid { get; }
+    public List<string> Errors { get; }
+
+    public OrderValidationEventArgs(Order order, bool isValid, List<string> errors)
+    {
+        Order = order;
+        this.isValid = isValid;
+        Errors = errors;    
+    }
+}
