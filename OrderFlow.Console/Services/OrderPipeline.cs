3a595583eccc0728aecd8d77b68e7678d17825a3
igorptak557788 <igor.ptak1@wp.pl>
2026-04-02 18:51:02 +0200

    Zadanie 2.1 Skonczone

--- OrderFlow.Console/Services/OrderPipeline.cs
+++ OrderFlow.Console/Services/OrderPipeline.cs
@@ -1,6 +1,36 @@
 ﻿namespace ConsoleApp1.Services;
 
+using ConsoleApp1.Models;
+
 public class OrderPipeline
 {
-    public int TEST;
+    private readonly OrderValidator _validator = new();
+    
+    public event EventHandler<OrderStatusChangedEventArgs>? StatusChanged;
+    public event EventHandler<OrderValidationEventArgs>? ValidationCompleted;
+
+    public void ProcessOrder(Order order)
+    {
+        //Walidacja
+        var errors = _validator.ValidateAll(order);
+        bool isValid = !errors.Any();
+        
+        ValidationCompleted?.Invoke(this, new OrderValidationEventArgs(order, isValid, errors));
+
+        if (!isValid) return;
+        
+        //Statusy
+
+        ChangeStatus(order, OrderStatus.Validated);
+        ChangeStatus(order, OrderStatus.Processing);
+        ChangeStatus(order, OrderStatus.Completed);
+    }
+
+    private void ChangeStatus(Order order, OrderStatus newStatus)
+    {
+        var oldStatus = order.Status;
+        order.Status = newStatus;
+        
+        StatusChanged?.Invoke(this, new OrderStatusChangedEventArgs(order, oldStatus, newStatus));
+    }
 }
